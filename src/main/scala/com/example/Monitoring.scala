51e25f165a7a5103d617545ce3799e77a5c3a5ef
Korneliusz <korneliusz.caputa@avaus.fi>
2015-04-22 08:52:42 +0200

    Change the format to console-based for now.

--- src/main/scala/com/example/Monitoring.scala
+++ src/main/scala/com/example/Monitoring.scala
@@ -3,15 +3,8 @@ package com.example
 import akka.actor.{ActorLogging, Actor, ActorRef, Terminated}
 import com.example.websocket.{WebSocketProducerActor, WebSocket}
 
-class Monitoring extends Actor with WebSocketProducerActor with ActorLogging {
-  val listeners: scala.collection.mutable.Set[ActorRef] = scala.collection.mutable.Set()
-
+class Monitoring extends Actor with ActorLogging {
   def receive = {
-    case WebSocket.Open(_, origin) =>
-      listeners add origin
-      context watch origin
-
-    case Terminated(origin) =>
-      listeners remove origin
+    case _ => {}
   }
 }
