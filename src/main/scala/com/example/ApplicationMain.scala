965781082989e4b4bc0b14d1e5542d2ca0555ae5
Korneliusz Caputa <helluinster@gmail.com>
2015-04-24 00:01:21 +0200

    Add basic killing messages.

--- src/main/scala/com/example/ApplicationMain.scala
+++ src/main/scala/com/example/ApplicationMain.scala
@@ -1,42 +1,36 @@
 package com.example
 
-import java.util.UUID
-
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{Kill, ActorSystem, PoisonPill, Props}
 import akka.io.IO
 import akka.pattern.ask
-import akka.routing.FromConfig
+import akka.routing.{Broadcast, FromConfig}
 import akka.util.Timeout
-import com.example.api.{OrderStatusDispatcher, ApiActor}
-import com.example.domain.actors.{Kitchen, Checkout}
-import com.example.domain.messages.Order
+import com.example.api.{ApiActor, OrderStatusDispatcher}
+import com.example.domain.actors.{Checkout, Kitchen}
 import spray.can.Http
 
 import scala.concurrent.duration._
-import scala.util.Random
 
 object ApplicationMain extends App {
   implicit val system = ActorSystem("McBurgerSystem")
+
+  implicit val timeout = Timeout(5.seconds)
+  // create and start our service actor
+  val statusDispatcher = system.actorOf(Props[OrderStatusDispatcher], "status-dispatcher")
+  val checkout = system.actorOf(Props(classOf[Checkout], statusDispatcher), "checkout")
+  val kitchen = system.actorOf(FromConfig.props(Props(classOf[Kitchen], checkout)), "kitchen-roundrobin")
 
   def startApi(): Unit = {
     val service = system.actorOf(Props(classOf[ApiActor], statusDispatcher, kitchen), "api")
     IO(Http) ? Http.Bind(service, interface = "localhost", port = 8080)
   }
 
+  println("=== SENDING POISON PILL ===")
+  // Stop all routees and then the router.
+//  kitchen ! PoisonPill
 
-  // create and start our service actor
-  val statusDispatcher = system.actorOf(Props[OrderStatusDispatcher], "status-dispatcher")
-  val checkout = system.actorOf(Props(classOf[Checkout], statusDispatcher), "checkout")
+  // Stop all routees.
+  kitchen ! Broadcast(Kill)
 
-//  val kitchen = system.actorOf(Props(classOf[Kitchen], checkout), "kitchen")
-  val kitchen = system.actorOf(FromConfig.props(Props(classOf[Kitchen], checkout)), "kitchen-round-robin")
-  implicit val timeout = Timeout(5.seconds)
-  // start a new HTTP server on port 8080 with our service actor as the handler
-
-  (1 to 100) foreach {(_) =>
-    kitchen ! Order(UUID.randomUUID(), Random.nextInt(10), 0, 0,0,0,0)
-  }
-  println("========= SENT MESSAGES =========")
-
-//  startApi()
+  //  startApi()
 }
