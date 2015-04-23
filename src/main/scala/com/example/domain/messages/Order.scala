83e078412c662da35102e4fd8dd156decd1ef35e
Korneliusz <korneliusz.caputa@avaus.fi>
2015-04-23 15:05:18 +0200

    Add Spray POST endpoint.

--- src/main/scala/com/example/domain/messages/Order.scala
+++ src/main/scala/com/example/domain/messages/Order.scala
@@ -2,37 +2,47 @@ package com.example.domain.messages
 
 import java.util.UUID
 
+import com.example.messages.OrderRequest
+
+object Order {
+  def fromRequest(request: OrderRequest) =
+    Order(UUID.randomUUID(),
+      request.sandwiches,
+      request.fries,
+      request.salads,
+      request.drinks,
+      request.coffees,
+      request.shakes)
+}
+
 case class Order(orderId: UUID,
                  sandwiches: Int,
                  fries: Int,
                  salads: Int,
                  drinks: Int,
                  coffees: Int,
-                 shakes: Int) 
+                 shakes: Int)
   extends OrderMessage {
-    def add(sandwiches: Int = 0,
-            fries: Int = 0,
-            salads: Int = 0,
-            drinks: Int = 0,
-            coffees: Int = 0,
-            shakes: Int = 0) =  
-         Order(
-           this.orderId, 
-           this.sandwiches + sandwiches, 
-           this.fries + fries, 
-           this.salads + salads, 
-           this.drinks + drinks, 
-           this.coffees + coffees, 
-           this.shakes + shakes)
+  def add(sandwiches: Int = 0,
+          fries: Int = 0,
+          salads: Int = 0,
+          drinks: Int = 0,
+          coffees: Int = 0,
+          shakes: Int = 0) =
+    Order(
+      this.orderId,
+      this.sandwiches + sandwiches,
+      this.fries + fries,
+      this.salads + salads,
+      this.drinks + drinks,
+      this.coffees + coffees,
+      this.shakes + shakes)
 
-    def hasAllProducts(required: Order) = 
-      sandwiches == required.sandwiches &&
+  def hasAllProducts(required: Order) =
+    sandwiches == required.sandwiches &&
       fries == required.fries &&
       salads == required.salads &&
       drinks == required.drinks &&
       coffees == required.coffees &&
       shakes == required.shakes
-
-    override def toString() = 
-      s"[$orderId] sandwiches: $sandwiches, fries: $fries, salads: $salads, drinks: $drinks, coffees: $coffees, shakes: $shakes"
-  }
+}
